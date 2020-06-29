5d917981c22ae04583ffb13f8fa3674a632cdbcb
Scott Harris <scottsharris@yahoo.com>
2020-06-29 10:37:27 -0700

    created burger controller

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -13,3 +13,43 @@ router.get("/", (req, res) => {
         res.render("index", hbsObject);
     });
 });
+
+router.post("/api/burgers", (req, res) => {
+    burger.create([
+        "name", "eaten"
+    ], [
+        req.body.name, req.body.eaten
+    ], (result) => {
+        res.json({ id: result.insertId });
+    });
+});
+
+router.put("/api/burgers/:id", (req, res) => {
+    const condition = `id = ${req.params.id}`;
+
+    console.log("condition", condition);
+
+    burger.update({
+        eaten: req.body.eaten
+    }, condition, (result) => {
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+router.delete("/api/burgers/:id", (req, res) => {
+    const condition = `id = ${req.params.id}`;
+
+    burger.delete(condition, (result) => {
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+module.exports = router;
