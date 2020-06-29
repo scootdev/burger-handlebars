31153b770ee2a20209cff957593d541fad420ae7
Scott Harris <scottsharris@yahoo.com>
2020-06-29 10:27:18 -0700

    created base handlebars files

---
+++ controllers/burgerController.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const router = express.Router();
+
+const burger = require("../models/burger");
+
+router.get("/", (req, res) => {
+    burger.all((data) => {
+        const hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
